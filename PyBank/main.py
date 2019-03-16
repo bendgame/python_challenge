21fe9f4fcb2eb1057aea2fdf44297ee3d136e65b
bendgame <44721179+bendgame@users.noreply.github.com>
2019-03-16 14:02:33 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,19 +1,5 @@
 #Eric Kleppen
-
-#this is main.py in the pybank
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
-#The average of the changes in "Profit/Losses" over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-# Financial Analysis
-# ----------------------------
-# Total Months: 86
-# Total: $38382578
-# Average  Change: $-2315.12
-# Greatest Increase in Profits: Feb-2012 ($1926159)
-# Greatest Decrease in Profits: Sep-2013 ($-2196167)
-
+#hw3
 
 import csv
 import os
